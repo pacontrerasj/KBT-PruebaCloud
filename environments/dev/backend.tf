1682e8e6b85515e8e1025a9f672ccb4a99e64910
pacontrerasj <pa.contrerasj@duocuc.cl>
2026-05-21 00:21:48 -0400

    fix: use_lockfile mejor practica que poner la tabla dynamo

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -3,6 +3,6 @@ terraform {
     bucket         = "kbt-technova-terraform-state"
     key            = "dev/terraform.tfstate"
     region         = "us-east-1"
-    dynamodb_table = "kbt-technova-terraform-locks"
+    use_lockfile = true
   }
 }
