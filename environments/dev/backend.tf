d4e5c71cdd3b2f53b3faca4b0d9384255351b200
pacontrerasj <pa.contrerasj@duocuc.cl>
2026-05-22 03:09:18 -0400

    fix: fmt recursive error

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket         = "kbt-technova-terraform-state"
-    key            = "dev/terraform.tfstate"
-    region         = "us-east-1"
-    encrypt        = true
+    bucket  = "kbt-technova-terraform-state"
+    key     = "dev/terraform.tfstate"
+    region  = "us-east-1"
+    encrypt = true
   }
 }
